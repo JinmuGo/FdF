023aca01a6ab32eac8ea067f0122d088a71c65b7
jgo-42 <jingmuio@gmail.com>
2023-01-06 16:04:58 +0900

    :art: Refactor: 프로젝트 계층 변경 draw, hooks, utils 폴더 추가.

--- src/src_m/draw/draw_mod.c
+++ src/src_m/draw/draw_mod.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_mod.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgo <jgo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jgo <jgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 19:28:51 by jgo               #+#    #+#             */
-/*   Updated: 2022/12/28 19:29:18 by jgo              ###   ########.fr       */
+/*   Updated: 2023/01/06 15:41:22 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ void	spherize(t_map *map, t_dot *points)
 	int	i;
 	int	rad[3];
 
-	rad[X] = map->ang[X] * M_PI / 180;
-	rad[Y] = map->ang[Y] * M_PI / 180;
-	rad[Z] = map->ang[Z] * M_PI / 180;
+	rad[X] = map->angle[X] * M_PI / 180;
+	rad[Y] = map->angle[Y] * M_PI / 180;
+	rad[Z] = map->angle[Z] * M_PI / 180;
 	i = 0;
-	while (i < map->len)
+	while (i < map->total_len)
 	{
-		points[i].axis[X] = (map->radius + points[i].axis[Z]) * \
-		cos(points[i].polar[LONG]) * sin(points[i].polar[LAT]);
-		points[i].axis[Y] = (map->radius + points[i].axis[Z]) * \
-		sin(points[i].polar[LONG]) * sin(points[i].polar[LAT]);
-		points[i].axis[Z] = (map->radius + points[i].axis[Z]) * \
-		cos(points[i].polar[LAT]);
+		// points[i].axis[X] = (map->radius + points[i].axis[Z]) * \
+		// cos(points[i].polar[LONG]) * sin(points[i].polar[LAT]);
+		// points[i].axis[Y] = (map->radius + points[i].axis[Z]) * \
+		// sin(points[i].polar[LONG]) * sin(points[i].polar[LAT]);
+		// points[i].axis[Z] = (map->radius + points[i].axis[Z]) * \
+		// cos(points[i].polar[LAT]);
 		i++;
 	}
 }
